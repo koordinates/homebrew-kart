080ee058ebd50561a0ebd4057418bdaf69c663ae
Robert Coup <robert@coup.net.nz>
2020-03-27 13:46:10 +0000

    Update for v0.3.1 release

--- Casks/sno.rb
+++ Casks/sno.rb
@@ -1,6 +1,6 @@
 cask 'sno' do
-  version '0.3.0'
-  sha256 '5f2dc6baa133cccf96540f71c1ad1e7eb216518c58903ae56567a4f8ac441efb'
+  version '0.3.1'
+  sha256 '0879e38ecb605888390056a6289005f3c679555beae5a95d17e9169bc431fb43'
 
   # github.com/koordinates/sno was verified as official when first introduced to the cask
   url "https://github.com/koordinates/sno/releases/download/v#{version}/Sno-#{version}.pkg"
@@ -8,7 +8,7 @@ cask 'sno' do
   name 'Sno'
   homepage 'https://sno.earth/'
 
-  pkg 'Sno-0.3.0.pkg'
+  pkg "Sno-#{version}.pkg"
 
   uninstall pkgutil: 'com.koordinates.Sno.SnoCore'
 end
