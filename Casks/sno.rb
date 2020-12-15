2ff57092ace1dc431f18aa4a683c18e2994cc582
Andrew Olsen <andrew.olsen@koordinates.com>
2020-12-15 15:02:00 +1300

    Update to release v0.7.0

--- Casks/sno.rb
+++ Casks/sno.rb
@@ -1,6 +1,6 @@
 cask 'sno' do
-  version '0.6.0'
-  sha256 '36d2ab27a0d0833b9f73427b3babb791026c4db7fc9f9d135234419d27682ea4'
+  version '0.7.0'
+  sha256 '4bc4b8f90e74bd7727c5bb050d6bcb874c40aa165ecf2105efdd15b4d7221100'
 
   # github.com/koordinates/sno was verified as official when first introduced to the cask
   url "https://github.com/koordinates/sno/releases/download/v#{version}/Sno-#{version}.pkg"
