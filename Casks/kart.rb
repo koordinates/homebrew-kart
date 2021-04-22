993b7d42219f884add0eb1c3b7cc8849ba60199d
Andrew Olsen <andrew.olsen@koordinates.com>
2021-04-22 14:55:18 +1000

    Rename sno to kart

--- Casks/kart.rb
+++ Casks/kart.rb
@@ -1,14 +1,14 @@
-cask 'sno' do
+cask 'kart' do
   version '0.8.0'
   sha256 '2b43e59f686de010c5e1ddf1a42290b79b1b459fcf9b6e41ddc73e631339b2a8'
 
   # github.com/koordinates/sno was verified as official when first introduced to the cask
-  url "https://github.com/koordinates/sno/releases/download/v#{version}/Sno-#{version}.pkg"
+  url "https://github.com/koordinates/sno/releases/download/v#{version}/Kart-#{version}.pkg"
   appcast 'https://github.com/koordinates/sno/releases.atom'
-  name 'Sno'
-  homepage 'https://sno.earth/'
+  name 'Kart'
+  homepage 'https://kartproject.org'
 
-  pkg "Sno-#{version}.pkg"
+  pkg "Kart-#{version}.pkg"
 
   uninstall pkgutil: 'com.koordinates.Sno.SnoCore'
 end
