8e204603fa3735f679391fceb4d5d296fba0dcd7
Robert Coup <robert@coup.net.nz>
2021-04-23 12:18:04 +0100

    Give up on renaming support for sno -> kart
    * formula_renames.json doesn't work for Casks.
    * other patterns (eg: zoomus -> zoom) end up in an uninstaller mess.
    * keep two with a message in the meantime.

--- Casks/sno.rb
+++ Casks/sno.rb
@@ -1,17 +1,27 @@
-cask "kart" do
+cask "sno" do
   version "0.9.0"
   sha256 "068d92c3c052e4e93b379fd701f813f61cd7dfdb49910c5dff1a089816319d1a"
 
   url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}.pkg",
       verified: "github.com/koordinates/kart/"
   appcast "https://github.com/koordinates/kart/releases.atom"
-  name "Kart"
+  name "Sno (now Kart)"
   desc "Distributed version-control for geospatial and tabular data"
   homepage "https://kartproject.org/"
 
-  conflicts_with cask: "sno"
+  conflicts_with cask: "kart"
 
   pkg "Kart-#{version}.pkg"
 
   uninstall pkgutil: "com.koordinates.Sno.SnoCore"
+
+  caveats <<~EOS
+    Sno has been renamed to Kart.
+
+    In the meantime, sno will install kart for you, but you should update your scripts.
+
+    To migrate now, do:
+      brew uninstall sno
+      brew install kart
+  EOS
 end
