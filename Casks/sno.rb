41f5f82ff3c2229bb6011e33c20a4ded87fa5163
user <user@user.use>
2023-02-13 21:52:22 +1300

    Update to release v0.12.2

--- Casks/sno.rb
+++ Casks/sno.rb
@@ -1,6 +1,6 @@
 cask "sno" do
-  version "0.12.1"
-  sha256 "9d155beef6b526c526a5852df5c940c9cee9eea2192b2284a286fb9c5833fa41"
+  version "0.12.2"
+  sha256 "8b23df08641b1ec946370cd52c66170d5d1ac73c1458d725dd19d238913df163"
 
   url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}-macOS-x86_64.pkg",
       verified: "github.com/koordinates/kart/"
