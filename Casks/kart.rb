0b1c9541819d5b89095a5ad6865f7932354883f9
Robert Coup <robert@coup.net.nz>
2023-10-25 11:20:55 +0100

    Support for arm64 releases.

--- Casks/kart.rb
+++ Casks/kart.rb
@@ -1,8 +1,11 @@
 cask "kart" do
   version "0.14.2"
-  sha256 "88550e3c022c88293220f9a25ca30b8e5a770c48986bdd30457224d6820c0e87"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}-macOS-x86_64.pkg",
+  sha256 arm:   "",
+         intel: "88550e3c022c88293220f9a25ca30b8e5a770c48986bdd30457224d6820c0e87"
+
+  url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}-macOS-#{arch}.pkg",
       verified: "github.com/koordinates/kart/"
   appcast "https://github.com/koordinates/kart/releases.atom"
   name "Kart"
@@ -11,7 +14,7 @@ cask "kart" do
 
   conflicts_with cask: "sno"
 
-  pkg "Kart-#{version}-macOS-x86_64.pkg"
+  pkg "Kart-#{version}-macOS-#{arch}.pkg"
 
   uninstall pkgutil: "com.koordinates.Sno.SnoCore"
 end
--- Casks/sno.rb
+++ Casks/sno.rb
@@ -1,8 +1,11 @@
 cask "sno" do
   version "0.14.2"
-  sha256 "88550e3c022c88293220f9a25ca30b8e5a770c48986bdd30457224d6820c0e87"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}-macOS-x86_64.pkg",
+  sha256 arm:   "",
+         intel: "88550e3c022c88293220f9a25ca30b8e5a770c48986bdd30457224d6820c0e87"
+
+  url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}-macOS-#{arch}.pkg",
       verified: "github.com/koordinates/kart/"
   appcast "https://github.com/koordinates/kart/releases.atom"
   name "Sno (now Kart)"
@@ -11,7 +14,7 @@ cask "sno" do
 
   conflicts_with cask: "kart"
 
-  pkg "Kart-#{version}-macOS-x86_64.pkg"
+  pkg "Kart-#{version}-macOS-#{arch}.pkg"
 
   uninstall pkgutil: "com.koordinates.Sno.SnoCore"
 
