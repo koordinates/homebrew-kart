e1bb77c0f0e720b419f3af285ed145b2de8230a4
Andrew Olsen <andrew.olsen@koordinates.com>
2023-12-04 14:30:04 +1300

    Update to release v0.15.0

--- Casks/kart.rb
+++ Casks/kart.rb
@@ -1,9 +1,9 @@
 cask "kart" do
-  version "0.14.2"
+  version "0.15.0"
   arch arm: "arm64", intel: "x86_64"
 
-  sha256 arm:   "",
-         intel: "88550e3c022c88293220f9a25ca30b8e5a770c48986bdd30457224d6820c0e87"
+  sha256 arm:   "c931f8ad4375518318aabd2b8d23a585fab0d4c642e113d1fd28bf9443a4cff2",
+         intel: "d363da90d17242fc2adc5e258f9cb1a1f977010b9583fb325a2f662754b6fa68"
 
   url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}-macOS-#{arch}.pkg",
       verified: "github.com/koordinates/kart/"
