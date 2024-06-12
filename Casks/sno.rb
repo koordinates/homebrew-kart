a2ea531c587d7651fdc5e8f63972b74a0bcc6d75
Andrew Olsen <andrew.olsen@koordinates.com>
2024-06-12 13:52:07 +1200

    Update to release v0.15.2

--- Casks/sno.rb
+++ Casks/sno.rb
@@ -1,9 +1,9 @@
 cask "sno" do
-  version "0.15.1"
+  version "0.15.2"
   arch arm: "arm64", intel: "x86_64"
 
-  sha256 arm:   "70dd9b3a0a4cf8d0d141ba7634de148128185b6247475a97c65d96c3420eaebf",
-         intel: "9316a4ca680c711851d1a433f408353272d2cea1ef5286dc47b2df47eaa36a07"
+  sha256 arm:   "442b1e9f628d9cb73873919c517c818ac8a9d0ede4968deb4464f289c59fd597",
+         intel: "9340fb7afbc9191e48d2949b006904d6d2198c27caf0be35c621a226c17364ab"
 
   url "https://github.com/koordinates/kart/releases/download/v#{version}/Kart-#{version}-macOS-#{arch}.pkg",
       verified: "github.com/koordinates/kart/"
